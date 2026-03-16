6c62c66246d04e6c2636d64c8032090596eaadd4
ReaganOswald <reagan.marie.oswald@gmail.com>
2026-03-15 23:47:43 -0400

    Update exploratory_analysis_day4_2h.py

--- Code/exploratory_analysis_day4_2h.py
+++ Code/exploratory_analysis_day4_2h.py
@@ -322,15 +322,3 @@ if __name__ == "__main__":
 
 
 
-        
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
